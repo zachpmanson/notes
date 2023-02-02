a790e5ddeb92f2865a0b8852034e6ed8b5d8d83b
Zach Manson <zachpmanson@gmail.com>
2023-02-02 10:22:23 +0800

    Add static files

--- build.sh
+++ build.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 
 source ./venv/bin/activate
+rm ./site/*.html
 echo Activated venv
 python3 ./generator/generator.py
 deactivate
 echo Deactivated venv
 mkdir -p ./site/static/media
-rm -r ./site/static/media/*
+rm -r ./site/static/*
+cp -R ./static/ ./site/static/
 cp -R ./notes/Media/ ./site/static/media/
 echo Done!
