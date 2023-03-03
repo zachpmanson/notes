e6178df65cee5612529833e4b0fdab3cdd8ab36e
Zach Manson <zachpmanson@gmail.com>
2023-03-03 09:01:50 +0800

    Update notes

--- update-notes.sh
+++ update-notes.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
+git fetch
 git stash
-set -e
 git pull
 git stash apply
-git add notes
-git commit -m "Auto-commit notes update"
-git push
+
+
+if [[ `git status --porcelain` ]]; then
+    # if there are changes to notes, commit them and push them
+    git add notes
+    git commit -m "Auto-commit notes update"
+    git push
+# else
+  # No changes
+fi
+
 ./deploy.sh
