3fb9c698362ed85981a45d253265f74595f0f623
Zach Manson <zachpmanson@gmail.com>
2023-11-19 19:29:04 +1100

    Add untracked files to git commit msg

--- update-notes.sh
+++ update-notes.sh
@@ -3,8 +3,9 @@ set -e
 git pull --autostash
 
 if [[ $(git status --porcelain) ]]; then
+    g add -N notes # add new files as tracked files
     # if there are changes to notes, commit them and push them
-    FILESCHANGED=$(git diff --name-only notes | sed 's/.*/"&"/' | xargs -n1 basename | tr "\n" " ")
+    FILESCHANGED=$(git diff --name-only notes | sed 's/.*/"&"/' | xargs -n1 basename | sed 's/\.md//' | tr "\n" " ")
     CLEANFILENAMES=${FILESCHANGED% }
     git add notes
     git commit -m "Update notes: $CLEANFILENAMES"
