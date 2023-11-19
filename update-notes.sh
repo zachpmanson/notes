6b33752e01621270438eebdfe7427de8182d0601
Zach Manson <zachpmanson@gmail.com>
2023-11-19 20:09:57 +1100

    Fix command alias

--- update-notes.sh
+++ update-notes.sh
@@ -3,7 +3,7 @@ set -e
 git pull --autostash
 
 if [[ $(git status --porcelain) ]]; then
-    g add -N notes # add new files as tracked files
+    git add -N notes # add new files as tracked files
     # if there are changes to notes, commit them and push them
     FILESCHANGED=$(git diff --name-only notes | sed 's/.*/"&"/' | xargs -n1 basename | sed 's/\.md//' | tr "\n" " ")
     CLEANFILENAMES=${FILESCHANGED% }
