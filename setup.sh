7620c45fe29dbe0a132a6286b1caac09eb458358
Zach Manson <zach.manson@bukalapak.com>
2023-11-20 16:28:45 +1100

    Fix venv location

--- setup.sh
+++ setup.sh
@@ -2,6 +2,6 @@
 mkdir site
 git worktree add site gh-pages
 
-python3 -m venv generator/venv 
-. ./generator/venv/bin/activate
+python3 -m venv venv 
+. ./venv/bin/activate
 python3 -m pip install -r generator/requirements.txt
