9bfa74bd80af1b64fe502e19597fec46f53bfc33
Zach Manson <zachpmanson@gmail.com>
2026-03-25 00:23:04 +1100

    Update notes: M generator/generator.py
    Affected files:
    generator/generator.py

--- generator/generator.py
+++ generator/generator.py
@@ -18,6 +18,7 @@ from markdown.extensions.codehilite import CodeHiliteExtension
 
 from generator.extensions.cite import CiteExtension
 from generator.extensions.backlink import BacklinkExtension
+from extensions.pathconverter import PathConverterExtension
 
 import generator.helpers as helpers
 
